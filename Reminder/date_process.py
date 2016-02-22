1134382b90ff0c45b3273e6d6137dcd59d328f30
shravan97 <shravanmurali@gmail.com>
2016-02-23 00:52:47 +0530

    added code to set cron elements in the set_cron_elements() function

--- Reminder/date_process.py
+++ Reminder/date_process.py
@@ -2,16 +2,39 @@ import time
 
 class dateProcessor:
 	"""class dateProcessor"""
-	def __init__(self , date):
+	def __init__(self , date , Time):
 		self.date = date
+		self.Time = Time
 
-	def extract_elements(self):
-		d = self.date
-		elements = d.split('-')
-		return elements
+	def extract_elements(self , k):
+		if k:
+			d = self.date
+			elements = d.split('-')
+			return elements
+		t = self.Time
+		return t.split(':')		
 
-	def set_cron_date(self):
-		inp = self.extract_elements(self.date)
+	def set_cron_elements(self):
+		inp_date = self.extract_elements(1)
+		inp_Time = self.extract_elements(0)
 		curr_yr = time.strftime("%y")
 		curr_month = time.strftime("%m")
 		curr_day = time.strftime("%d")
+		
+		cron_elements = {'time':[] , 'date':[]}
+		cron_elements['time'] = inp_Time
+
+		if int(inp_date[0])-int(curr_yr)-2000 >1:
+			return -1
+		elif int(inp_date[0])-int(curr_yr)-2000 ==1 :
+			if int(inp_date[1])-int(curr_month) > 0:
+				return -1
+			elif int(inp_date[1])-int(curr_month) == 0:
+				if int(inp_date[2])-int(curr_day) >=0:
+					return -1
+				else:
+					cron_elements['date'] = inp_date
+			else:
+				cron_elements['date'] = inp_date
+		else:
+			cron_elements['date'] = inp_date			
