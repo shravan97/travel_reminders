dd81d820a8c0218c9513f27f21adb0a4f51f0a31
shravan97 <shravanmurali@gmail.com>
2016-04-02 23:04:29 +0530

    added functions of previous file to run_cron.py

--- Reminder/run_cron.py
+++ Reminder/run_cron.py
@@ -1,18 +1,38 @@
 import getpass
 from crontab import CronTab
+from datetime import datetime
 
-# For now , set a cronjob in the name of user and later add root
-username = getpass.getuser()
 #Get existing cronjobs for user using subprocess to capture the output of 'crontab -l'
 class CronProcessor(object):
 	''' To added a new cronjob , to edit and delete a cronjob'''
-	
-	def __init__(self , possessions):
+
+	def validity(self,date_time):
+		curr = datetime.now()
+
+		if date_time<curr:
+			return 0
+		return 1	
+
+	def __init__(self , possessions , date_time):
 		self.numReminders = sum(1 for line in open('myReminders.txt' ,'r'))
 		self.possessions = possessions
 		self.cron = CronTab(user=username)
+		self.username = getpass.getuser()
 
-	def add_cron(self , date_time):
-		c = self.cron
-		job = c.new(command='python -id '+self.newnum+' alarm.py' , comment=str(self.newnum))
-		job.setall(date_time)
+		if validity(date_time):
+			self.isValid = True
+			self.date_time = date_time
+		else
+			self.isValid = False
+			print "Your reminder date is invalid"
+
+	def add_cron(self):
+		if self.isValid:
+			c = self.cron
+			job = c.new(command='python -id '+self.newnum+' alarm.py' , comment=str(self.newnum))
+			job.setall(self.date_time)
+			job.enable()
+			c.remove_all(time='* * * * *')
+			c.write()
+		else:
+			print "Your date is invalid"
