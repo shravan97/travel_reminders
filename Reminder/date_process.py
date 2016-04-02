d1f3ba3d59e31b40d8c7b377e5be17d3c9df335a
shravan97 <shravanmurali@gmail.com>
2016-04-02 18:48:24 +0530

    changed every date and time to datetime object

--- Reminder/date_process.py
+++ Reminder/date_process.py
@@ -1,42 +1,15 @@
-import time
-import datetime
+from datetime import datetime
 
 class date_time_Processor:
 	"""class for processing the input date and time and set the cron variables"""
-	def __init__(self , date , Time):
-		self.date = date
-		self.Time = Time
-
-	@staticmethod
-	def get_curr_date():
-		curr = [time.strftime("%y") , time.strftime("%m") ,time.strftime("%d")]
-		return curr
+	def __init__(self , DateTime):
+		self.DateTime = DateTime
 
 	def set_cron_elements(self):
-		curr = self.get_curr_date()
+		curr = datetime.now()
+		inp = self.DateTime
 
-		t=self.Time
-		d=self.date
-
-		inp_Time = [t.hour , t.minute]
-		inp_date = [d.year , d.month , d.day]
-
-		cron_elements = {'time':[] , 'date':[]}
-		cron_elements['time'] = inp_Time
-
-		if int(inp_date[0])-int(curr[0])-2000 >1:
+		if inp<curr:
 			return -1
-		elif int(inp_date[0])-int(curr[0])-2000 ==1 :
-			if int(inp_date[1])-int(curr[1]) > 0:
-				return -1
-			elif int(inp_date[1])-int(curr[1]) == 0:
-				if int(inp_date[2])-int(curr[2]) >=0:
-					return -1
-				else:
-					cron_elements['date'] = inp_date
-			else:
-				cron_elements['date'] = inp_date
 		else:
-			cron_elements['date'] = inp_date
-
-		return 	cron_elements
+			return self.DateTime
